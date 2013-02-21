cf704015553fe10ec413223111509d9cfdf85f55
Shalin <shalinshah1993@gmail.com>
2013-02-22 00:42:51 +0530

    The layout designed

---
+++ OurWindowsApp/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace OurWindowsApp
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Set the data context of the listbox control to the sample data
+            DataContext = App.ViewModel;
+            this.Loaded += new RoutedEventHandler(MainPage_Loaded);
+        }
+
+        // Load data for the ViewModel Items
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (!App.ViewModel.IsDataLoaded)
+            {
+                App.ViewModel.LoadData();
+            }
+        }
+
+        private void textBox1_GotFocus(object sender, RoutedEventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void textBox1_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (textBox1.Text == "")
+                textBox1.Text = "Search";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //NavigationService.Navigate(new Uri("/Page1.xaml",UriKind.RelativeOrAbsolute));
+            MessageBox.Show("Distance of the airport is :");
+        }
+
+       
+
+        
+    }
+}
